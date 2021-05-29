6520cd6916d1d136311a24b4d4da1af69262524c
Bryant Wooters <Jesusrox45@gmail.com>
2021-05-29 16:43:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const randomNumber = Math.floor((Math.random() * 6) + 1);
+const dice1 = "\images\dice1.png";
+const dice2 = "\images\dice2.png";
+const dice3 = "\images\dice3.png";
+const dice4 = "\images\dice4.png";
+const dice5 = "\images\dice5.png";
+const dice6 = "\images\dice6.png";
+
